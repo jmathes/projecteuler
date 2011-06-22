2c221f77a82f9d7cc5006ede7593bee38c07af96
Joe Mathes <joe@saucelabs.com>
2011-06-21 18:41:54 -0700

    22

---
+++ python/22.py
@@ -0,0 +1,17 @@
+import csv
+from pprint import pprint
+namefile = csv.reader(open('../names.txt', 'r'))
+for row in namefile:
+    names = row
+names.sort()
+
+lvals = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
+
+i = 0
+sum = 0
+for name in names:
+    i += 1
+    for letter in name.lower():
+        sum += i * lvals[letter]
+
+print sum
