80385d4c38d2b609357658def4d663c015b6688e
Joe Mathes <joe@saucelabs.com>
2011-11-08 13:29:39 -0800

    add someone else's faster sieve, expand primes list to 2999999, which is prime

--- python/primes.py
+++ python/primes.py
@@ -68,6 +68,7 @@ def expand_primes_list(limit=1000, limit_type = 'length'):
                 is_prime = False
                 break
         if is_prime:
+            print "added %s", test
             primes.append(test)
         test += 2
     pfile = open("primes.txt", "w")
@@ -98,6 +99,27 @@ def sieve(max):
         pfile = open("primes.txt", "w")
         pfile.writelines([str(prime) + '\n' for prime in primes])
     return primes
+
+def othersieve(max):
+    primes = sundaram3(max)
+    print len(primes)
+    print primes[-1]
+    pfile = open("primes.txt", "w")
+    pfile.writelines([str(prime) + '\n' for prime in primes])
+
+def sundaram3(max_n):
+    numbers = range(3, max_n+1, 2)
+    half = (max_n)//2
+    initial = 4
+
+    for step in xrange(3, max_n+1, 2):
+        for i in xrange(initial, half, step):
+            numbers[i-1] = 0
+        initial += 2*(step+1)
+
+        if initial > half:
+            return [2] + filter(None, numbers)
+
 
 def is_prime(test):
     max_prime = primes[len(primes) - 1]
